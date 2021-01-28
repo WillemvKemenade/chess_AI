5655c48bda931e6c2f19d0f492c597b916869d3f
WillemvKemenade <33415967+WillemvKemenade@users.noreply.github.com>
2021-01-29 00:35:08 +0100

    engine connection

--- Chess_test/main.py
+++ Chess_test/main.py
@@ -1,17 +1,20 @@
 import chess
 import chess.svg
+import os
 
 from PyQt5.QtSvg import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
 from stockfish import Stockfish
+import engine
 
 stockfish = Stockfish("stockfish_20090216_x64_bmi2.exe")
-stockfish.set_skill_level(20) #this goes from 0 to 20
+stockfish.set_skill_level(0) #this goes from 0 to 20
 
 chessBoard = chess.Board()
 stockfishMovesList = []
+
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -30,8 +33,8 @@ class MainWindow(QWidget):
 
     def playMove(self, inputText, player):
         if player == "UUAI":
-            stockfishMovesList.append(str(chessBoard.parse_san(inputText)))
-            chessBoard.push_san(inputText)
+            stockfishMovesList.append(str(inputText))
+            chessBoard.push(chess.Move.from_uci(str(inputText)))
         else:
             stockfishMovesList.append(inputText)
             chessBoard.push(chess.Move.from_uci(inputText))
@@ -42,12 +45,14 @@ class Terminal(QRunnable):
     chessWindow = None
     keepPlaying = None
     textDisplayed = None
+    count = 0
 
     def __init__(self, window):
         super().__init__()
         self.chessWindow = window
         self.keepPlaying = True
         self.textDisplayed = False
+        self.count = 0
 
     def run(self):
         self.runGame()
@@ -55,23 +60,36 @@ class Terminal(QRunnable):
     def runGame(self):
         while self.keepPlaying is True:
             self.keepPlaying = self.UUAIPlays()
+            self.count = self.count + 1
 
             if self.keepPlaying is True:
                 self.keepPlaying = self.StockfishPlays()
+                self.count = self.count + 1
 
     def UUAIPlays(self):
         # First we play(WHITE)
-        legalMove = False
+        # legalMove = False
+        #
+        # while legalMove is False:
+        #     try:
+        #         playerInput = input()
+        #         chessBoard.parse_san(playerInput)
+        #         legalMove = True
+        #     except:
+        #         print("Illegal move try again")
 
-        while legalMove is False:
-            try:
-                playerInput = input()
-                chessBoard.parse_san(playerInput)
-                legalMove = True
-            except:
-                print("Illegal move try again")
+        computer_color = 'w'
+        if len(stockfishMovesList) == 0:
+            move = 'None'
+            position = chess.STARTING_FEN
+        else:
+            move = stockfishMovesList[len(stockfishMovesList) - 1]
+            position = chessBoard.fen()
 
-        self.chessWindow.playMove(playerInput, "UUAI")
+        engine.Engine(move, position, computer_color)
+        moves = engine.Node(chessBoard)
+        best_move = moves.best_moves[0][0]
+        self.chessWindow.playMove(best_move, "UUAI")
         return self.endOfGameCheck("WHITE")
 
     def StockfishPlays(self):
@@ -88,9 +106,11 @@ class Terminal(QRunnable):
     def endOfGameCheck(self, player):
         if chessBoard.is_checkmate():
             print(player, "WINS!")
+            print("Move count", self.count)
             return False
         elif chessBoard.is_insufficient_material():
             print("IT IS A DRAW")
+            print("Move count", self.count)
             return False
         else:
             return True
